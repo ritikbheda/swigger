5b664d914bfbc08232ef9528bc99cd4cacb56463
ritik <ritik.bheda@gmail.com>
2020-07-06 21:41:48 -0400

    env added

--- server.js
+++ server.js
@@ -128,6 +128,6 @@ app.post("/register", (req, res) => {
 })
 
 
-app.listen('3000', () => {
+app.listen(process.env.PORT||3000, () => {
     console.log('this server is ruuning on port 3000');
 });

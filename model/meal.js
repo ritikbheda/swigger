3e195efb8f6364b00592686de00ca39f23d10b20
ritik <ritik.bheda@gmail.com>
2020-07-06 21:32:41 -0400

    validation done

---
+++ model/meal.js
@@ -0,0 +1,105 @@
+const meal = {
+    fakeData: [],
+
+    initDB(){
+        this.fakeData.push({
+            title: "Pasta",
+            price: 7,
+            topMeal: true,
+            imgPath: "https://images.pexels.com/photos/1437267/pexels-photo-1437267.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+            desc: "pasta from Mexico"
+        });
+        this.fakeData.push({
+            title: "Dhokla",
+            price: 17,
+            topMeal: true,
+            imgPath: "https://images.pexels.com/photos/1653877/pexels-photo-1653877.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+            desc: "Special dhokla from rice"
+        });
+        this.fakeData.push({
+            title: "Salad",
+            price: 12,
+            topMeal: true,
+            imgPath: "https://images.pexels.com/photos/750075/pexels-photo-750075.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+            desc: "Salad with world's best chef and best incredians"
+        });
+        this.fakeData.push({
+            title: "Biryani",
+            price: 20,
+            topMeal: true,
+            imgPath: "https://images.pexels.com/photos/1647163/pexels-photo-1647163.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+            desc: "Pakistani Chiken Biryani avaiable with special 'chutney' "
+        });
+        this.fakeData.push({
+            title: "Rabdi",
+            price: 18,
+            topMeal: true,
+            imgPath: "https://images.pexels.com/photos/1213710/pexels-photo-1213710.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+            desc: "Rabdi out of milk"
+        });
+        this.fakeData.push({
+            title: "",
+            price: 18,
+            topMeal: true,
+            imgPath: "https://images.pexels.com/photos/1128678/pexels-photo-1128678.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+            desc: ""
+        });
+        
+        this.fakeData.push({
+            title: "Frankie",
+            price: 18,
+            topMeal: true,
+            imgPath: "https://images.pexels.com/photos/1128678/pexels-photo-1128678.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+            desc: ""
+        });
+        
+        this.fakeData.push({
+            title: "South Indian Special",
+            price: 18,
+            topMeal: true,
+            imgPath: "https://images.pexels.com/photos/1128678/pexels-photo-1128678.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+            desc: ""
+        });
+        
+        this.fakeData.push({
+            title: "Noodles",
+            price: 18,
+            topMeal: true,
+            imgPath: "https://images.pexels.com/photos/2347311/pexels-photo-2347311.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+            desc: ""
+        });
+        
+        this.fakeData.push({
+            title: "Sushi",
+            price: 18,
+            topMeal: true,
+            imgPath: "https://images.pexels.com/photos/1148086/pexels-photo-1148086.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+            desc: ""
+        });
+        
+        this.fakeData.push({
+            title: "Fries",
+            price: 18,
+            topMeal: true,
+            imgPath: "https://images.pexels.com/photos/1583891/pexels-photo-1583891.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+            desc: ""
+        });
+    },
+
+
+    getAllMeals(){
+        return this.fakeData;
+    },
+
+    getTopMeal(){
+        let result = this.fakeData.filter(meal => meal.topMeal);
+        return result;
+    }
+
+
+
+}
+
+
+meal.initDB();
+module.exports = meal;
---
+++ server.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+const fs = require('fs');
+require('dotenv').config({path:"./config/keys.env"})
+var bodyParser = require('body-parser')
+  
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+const path = require('path');
+const meal = require('./model/meal');
+
+// app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+// app.set('view engine', 'handlebars');
+
+app.set("views", "./views");
+app.engine('.hbs', exphbs({extname: ".hbs"}));
+app.set('view engine', '.hbs');
+app.use(express.static(path.join(__dirname,'/public')));
+
+
+app.get('/', (req, res) =>{
+   // console.log('currently on hime page');
+        res.render('home',{
+            title: 'Grofers',
+            style: 'home.css',
+            topMeal: meal.getTopMeal()
+        });
+    })
+
+app.get('/mealpakages', (req, res) => {
+    res.render("meal", {
+        title: "meal",
+        style: "meal.css"
+
+    });
+    //onsole.log('you are on the mael pakages page.');
+});
+
+app.get('/login', (req, res) => {
+    res.render("login",{
+        title: "login",
+        style: "login.css"
+    });
+    //console.log('you are in login pafe');
+});
+
+
+app.get('/register', (req, res) =>{
+    // console.log('currently on hime page');
+         res.render('register',{
+             title: 'register',
+             style: "login.css",
+             err_user: "",
+             user:"",
+             err_email: "",
+             email:"",
+             err_pass: "",
+             err_conpass: "",
+             pass: "",
+             conpass:"",
+             //topMeal: meal.getTopMeal();
+         });
+     })
+ 
+app.post("/register", (req, res) => {
+    let er_user=[];
+    let er_email=[];
+    let er_pass=[];
+    let er_conpass=[];
+    let regex=/^[0-9a-zA-Z]+$/;
+    if(req.body.user===""){
+        er_user.push("You must enter your firstname.");
+    }
+
+    if(req.body.email===""){
+        er_email.push("You must enter a valid email address.");
+    }
+    if(req.body.password===""){
+        er_pass.push("You must enter a password.");
+    }
+    else if(req.body.password.length<7){
+        er_pass.push("Password must be atleast 7 characters.");
+    }
+    if(!(req.body.conpassword==req.body.password)){
+        er_conpass.push("Password does not match");
+    }
+    if( er_user.length>0 || er_email.length>0 || er_pass.length>0 ||er_conpass.length>0 ){
+        res.render("register",{
+            title:"customer registeration form",
+            err_user: er_user, style: "login.css",
+            user:req.body.user,
+            err_email: er_email,
+            email:req.body.email,
+            err_pass: er_pass,
+            err_conpass: er_conpass,
+            pass: req.body.pass,
+            conpass: req.body.conpass,
+
+        });
+    }
+    //there are no errors
+    else{
+
+        const sgMail = require('@sendgrid/mail');
+        sgMail.setApiKey(process.env.API);
+        const msg = {
+        to: `${req.body.email}`,
+        from: "ritikbheda13@gmail.com",
+        subject: 'Contact Us Form Submit',
+        html: 
+        `Hi ${req.body.user}<br> This is a registeration confirmation.
+        `,
+        };
+    
+        //Asynchornous operation (who don't know how long this will take to execute)
+        sgMail.send(msg)
+        .then(()=>{
+            res.redirect("/");
+        })
+        .catch(err=>{
+            console.log(`Error ${err}`);
+        });
+    
+
+
+    }
+})
+
+
+app.listen('3000', () => {
+    console.log('this server is ruuning on port 3000');
+});
